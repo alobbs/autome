d108e604314a13b3ddc06bb4e898c271cf38bffe
Alvaro Lopez Ortega <alvaro@gnu.org>
2016-04-17 12:30:58 +0200

    Adds a run-script script

---
+++ chief/chief-run-script.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import sys
+
+import cron
+
+# PYTHONPATH
+dir_chief = os.path.dirname(os.path.realpath(__file__))
+dir_base = os.path.realpath(__file__ + "/../..")
+sys.path += [dir_chief, dir_base]
+
+
+def main():
+    # Parse parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument("script")
+    args = parser.parse_args()
+
+    # Run script
+    m = cron.load(args.script)
+    logging.basicConfig(stream=sys.stdout)
+    m.run()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
