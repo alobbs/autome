04ddadccd0de4ae6fdb726e0b237473d894d64c0
Alvaro Lopez Ortega <alvaro@gnu.org>
2016-04-22 16:09:19 +0200

    Adopts the utils plugin

--- scripts/hosts-adblock.py
+++ scripts/hosts-adblock.py
@@ -1,19 +1,13 @@
 import os
-import shutil
-import tempfile
 
-when = '4am'
-lapse = '24h'
+import plugin
+
+util = plugin.get("util")
+when, lapse = '4am', '12h'
 GIT = "https://github.com/alobbs/hosts-adblock.git"
 
 
 def run():
-    orid = os.getcwd()
-    tmpd = tempfile.mkdtemp()
-    os.chdir(tmpd)
-
-    os.system('git clone --depth=1 %s' % GIT)
-    os.system("./hosts-adblock/update-upload.py")
-
-    os.chdir(orid)
-    shutil.rmtree(tmpd)
+    with util.work_in_tmpdir():
+        os.system('git clone --depth=1 %s' % GIT)
+        os.system("./hosts-adblock/update-upload.py")
