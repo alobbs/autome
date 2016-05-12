384aaef148c0e6cff04f0e6bd7cbb8357c2777ed
Alvaro Lopez Ortega <alvaro@gnu.org>
2016-05-12 18:16:35 +0200

    Renames script

--- scripts/rhos-prio.py
+++ scripts/rhos-prio.py
@@ -1,14 +1,16 @@
 import bridge
 import plugin
 
+PRIO = "rhos-prio-list@redhat.com"
 FWD_FROM = "aortega@redhat.com"
 FWD_TO = "alvaro@alobbs.com"
 
-lapse = 1300
+lapse = "5m"
 imap, smtp, tele = plugin.get("imap", "smtp", "telegram")
 
 
 def run():
-    for msg in imap.get_new_msgs('INBOX', '(TEXT alvaro@alobbs.com)'):
+    # [!!] for msg in imap.get_new_msgs('INBOX', '(TEXT alvaro@alobbs.com)'):
+    for msg in imap.get_new_msgs('INBOX', '(TO %s)' % PRIO):
         smtp.forward_msg(msg, FWD_FROM, FWD_TO)
         tele.send_me_msg(bridge.email.to_text(msg))
